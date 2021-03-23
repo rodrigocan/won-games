a7cf1e18b77e00d74b0ed3af734fcbfa26139fb8
Rodrigo Candido <rodrigocan@gmail.com>
2021-03-23 15:55:34 -0300

    (142) Cfg ThemeProvider, TS support with .d.ts  #5

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,9 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`@font-face {font-family: 'Poppins';font-style: normal;
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
     font-weight: 300;
     font-display: swap;
     src: local(''),
@@ -33,17 +36,17 @@ const GlobalStyles = createGlobalStyle`@font-face {font-family: 'Poppins';font-s
     -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
-  body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
 `
 
 export default GlobalStyles
