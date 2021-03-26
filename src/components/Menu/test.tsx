273f4728dd4851c9a125cd97aaaa76587ce22f2f
Rodrigo Candido <rodrigocan@gmail.com>
2021-03-26 15:42:34 -0300

    (164) Create Menu - Part 7 #10

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -13,7 +13,7 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
   })
 
-  it('should render the open/close mobile menu', () => {
+  it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
     // selecionar o nosso MenuFull
@@ -32,5 +32,23 @@ describe('<Menu />', () => {
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="rodrigo" />)
+
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 })
