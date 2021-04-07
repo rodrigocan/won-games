90ea99bce2046cd07e57cf2e776acb8ac09ab447
Rodrigo Candido <rodrigocan@gmail.com>
2021-04-07 15:41:12 -0300

    (206) HomePage-Pt9-Connect data to page #23

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
@@ -11,7 +13,6 @@ export default function Index(props: any) {
 // getStaticProps => gerar estático em build time
 // getServerSideProps => gerar via ssr a cada request
 // getInitialProps => gerar via ssr a cada request
-
 export function getServerSideProps() {
   // faz lógica
   // pode ser buscar dados numa API
@@ -20,7 +21,15 @@ export function getServerSideProps() {
   // retorno dos dados
   return {
     props: {
-      heading: 'Olha eu aqui!!!!1234 ssr222'
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcomingGames: gamesMock,
+      upcomingHighlight: highlightMock,
+      upcomingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighlight: highlightMock
     }
   }
 }
