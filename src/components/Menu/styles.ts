b0ec27dd297cbbe03602e834628e1c09d76ecf0d
Rodrigo Candido <rodrigocan@gmail.com>
2021-04-08 12:25:37 -0300

    (207|208) HomePage-Pts 10-11-Styles and tests #23

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -43,8 +43,8 @@ export const MenuGroup = styled.div`
 export const MenuNav = styled.div`
   ${({ theme }) => css`
     ${media.greaterThan('medium')`
-      margin-left: ${theme.spacings.small};
-    `}
+			margin-left: ${theme.spacings.small};
+		`}
   `}
 `
 
@@ -91,7 +91,8 @@ export const MenuFull = styled.nav<MenuFullProps>`
     flex-direction: column;
     justify-content: space-between;
     background: ${theme.colors.white};
-    position: absolute;
+    position: fixed;
+    z-index: ${theme.layers.menu};
     top: 0;
     bottom: 0;
     left: 0;
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,32 +1,56 @@
-// import { screen } from '@testing-library/react'
-// import { renderWithTheme } from 'utils/tests/helpers'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-// import Home from '.'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-// describe('<Home />', () => {
-it('should render menu and footer', () => {
-  expect(1 + 1).toBe(2)
-  // renderWithTheme(<Home />)
+import Home from '.'
 
-  // expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-  // expect(
-  //   screen.getByRole('heading', { name: /contact us/i })
-  // ).toBeInTheDocument()
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcomingGames: [gamesMock[0]],
+  upcomingHighlight: highlightMock,
+  upcomingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighlight: highlightMock
+}
+
+describe('<Home />', () => {
+  it('should render home page', () => {
+    renderWithTheme(<Home {...props} />)
+
+    // menu
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    // footer
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    // logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcoming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    // highlight
+    expect(screen.getAllByText(/red dead is back!/i)).toHaveLength(3)
+  })
 })
-
-//   it('should render the sections', () => {
-//     // renderWithTheme(<Home />)
-
-//     // expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
-//     // expect(
-//     //   screen.getByRole('heading', { name: /most popular/i })
-//     // ).toBeInTheDocument()
-//     // expect(
-//     //   screen.getByRole('heading', { name: /upcoming/i })
-//     // ).toBeInTheDocument()
-//     // expect(
-//     //   screen.getByRole('heading', { name: /free games/i })
-//     // ).toBeInTheDocument()
-//   })
-// })
-export {}
