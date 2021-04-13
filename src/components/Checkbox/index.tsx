8c678655d1acd676d2c012f19355374cb191a64e
Rodrigo Candido <rodrigocan@gmail.com>
2021-04-13 08:57:31 -0300

    (214) Checkbox - Add static input and label #25

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Checkbox = () => (
+  <S.Wrapper>
+    <input id="action" type="checkbox" />
+
+    <label htmlFor="action">Action</label>
+  </S.Wrapper>
+)
+
+export default Checkbox
