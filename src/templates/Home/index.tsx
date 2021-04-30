eba3b0867e1e75edfb0655e76739e699bf7d0d2d
Rodrigo Candido <rodrigocan@gmail.com>
2021-04-29 23:36:44 -0300

    (339) Creating query for Games (Explore page)

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -10,7 +10,6 @@ import Showcase from 'components/Showcase'
 import * as S from './styles'
 
 export type HomeTemplateProps = {
-  data: string
   banners: BannerProps[]
   newGames: GameCardProps[]
   mostPopularHighlight: HighlightProps
