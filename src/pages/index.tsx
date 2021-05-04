b2d3372d03c1b8a1c97bd91b05b726bee135a8e0
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-04 09:35:34 -0300

    (366) Dynamic Home: today date variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 import { initializeApollo } from 'utils/apollo'
-import { QueryHome } from 'graphql/generated/QueryHome'
+import { QueryHome, QueryHomeVariables } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
-import { bannerMapper, gamesMapper } from 'utils/mappers'
-import { highlightMapper } from '../utils/mappers'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -17,10 +16,14 @@ export default function Index(props: HomeTemplateProps) {
 // getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois da primeira request)
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
+  const TODAY = new Date().toISOString().slice(0, 10) // 2021-01-27
 
   const {
     data: { banners, newGames, upcomingGames, freeGames, sections }
-  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+  } = await apolloClient.query<QueryHome, QueryHomeVariables>({
+    query: QUERY_HOME,
+    variables: { date: TODAY }
+  })
 
   return {
     props: {
