c32328846072f0493a88431d2d30d31f7e76053f
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-12 22:12:24 -0300

    (403) Add initial structure to useCart and CartProvider

---
+++ src/hooks/use-cart/index.tsx
@@ -0,0 +1,22 @@
+import { useContext, createContext } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type CartContextData = {}
+
+export const CartContextDefaultValues = {}
+
+export const CartContext = createContext<CartContextData>(
+  CartContextDefaultValues
+)
+
+export type CartProviderProps = {
+  children: React.ReactNode
+}
+
+const CartProvider = ({ children }: CartProviderProps) => {
+  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>
+}
+
+const useCart = () => useContext(CartContext)
+
+export { CartProvider, useCart }
