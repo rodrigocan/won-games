8e49567f17a298732b7fadf226962286f2d27012
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-18 21:18:56 -0300

    (452) Send forgot password email

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -40,15 +40,30 @@ export const FormLink = styled.div`
     font-size: ${theme.font.sizes.small};
     color: ${theme.colors.black};
     text-align: center;
+
     a {
       color: ${theme.colors.secondary};
       text-decoration: none;
       border-bottom: 0.1rem solid ${theme.colors.secondary};
       transition: color, border, ${theme.transition.fast};
+
       &:hover {
         color: ${darken(0.1, theme.colors.secondary)};
         border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
       }
+    }
+  `}
+`
+
+export const FormSuccess = styled.p`
+  ${({ theme }) => css`
+    text-align: center;
+    color: ${theme.colors.black};
+    font-size: ${theme.font.sizes.small};
+
+    svg {
+      color: ${theme.colors.secondary};
+      width: 2.4rem;
     }
   `}
 `
