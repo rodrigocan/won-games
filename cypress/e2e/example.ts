1013f8bbcd7e267f53928cbce516eda06a9703d7
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-26 22:48:45 -0300

    (555) Add start-server-and-test to Cypress

--- cypress/e2e/example.ts
+++ cypress/e2e/example.ts
@@ -1,12 +1,12 @@
 /// <reference path="../support/index.d.ts" />
 
-describe('Cypress TS', () => {
+describe.skip('Cypress TS', () => {
   it('should go to Google', () => {
     cy.google()
   })
 
   it('should change light/dark theme on willian justen site', () => {
-    cy.visit('http://willianjusten.com.br')
+    cy.visit('https://willianjusten.com.br')
 
     cy.findByTitle(/Mudar o tema/i).click()
     cy.get('.light').should('exist')
