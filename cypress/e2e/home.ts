20e43cce53a170b561eba2bb0742b9de979207de
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-27 09:06:08 -0300

    (558) Using click and wait to animation on banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -8,6 +8,17 @@ describe('Home Page', () => {
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /biomutant/i })
       cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /the witcher 3/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+
+      cy.findByRole('heading', { name: /cyberpunk 2077/i })
+      cy.findByRole('link', { name: /buy now/i })
     })
   })
 })
