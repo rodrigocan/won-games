9e45be0b82070b5dc8c60ce30ad6e7108e813ebd
Rodrigo Candido <rodrigocan@gmail.com>
2021-05-27 09:15:37 -0300

    (559.a) Create shouldRenderBanner custom command

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -2,10 +2,8 @@
 
 describe('Home Page', () => {
   it('should render home sections', () => {
-    // visitar a página
     cy.visit('/')
 
-    // selecionou os banners
     cy.shouldRenderBanner()
   })
 })
