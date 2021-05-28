976db18e3db2f29eb71d991ce75d7ad722fd46f4
Rodrigo Cândido <rodrigocan@gmail.com>
2021-05-28 18:30:27 -0300

    (573) Add test to show more

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -8,6 +8,10 @@ import {
 } from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
+  before(() => {
+    cy.visit('/games')
+  })
+
   it('should render filters columns', () => {
     cy.visit('/games')
 
@@ -20,5 +24,11 @@ describe('Explore Page', () => {
     cy.getFields(priceFields)
     cy.getFields(platformFields)
     cy.getFields(genreFields)
+  })
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
   })
 })
