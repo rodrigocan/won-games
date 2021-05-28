df51d12ae955f547d3d926ef37c386dd2399ec85
Rodrigo Cândido <rodrigocan@gmail.com>
2021-05-28 13:12:15 -0300

    (561) Using assertions

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,11 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
+
 declare namespace Cypress {
   /**
    * Custom command to visit google page
@@ -16,5 +21,13 @@ declare namespace Cypress {
    */
   interface Chainable {
     shouldRenderBanner(): Chainable<Element>
+  }
+
+  /**
+   * Custom command to check showcase in page
+   * @example cy.shouldRenderShowcase()
+   */
+  interface Chainable {
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
